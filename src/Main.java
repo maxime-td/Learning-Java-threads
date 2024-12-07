4a66a5bdf991240265dacb43eb9d70a25252b5a1
Maxime TP <maxime.thoumin@protonmail.com>
2024-12-07 09:24:39 +0100

    Main class translated

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,29 @@
 public class Main {
 
     public static void main(String[] args) {
-        int nbProducteurs = 50; // Nb de producteurs
-        int nbConsommateurs = 2; // Nb de consommateurs
 
-        Stock stock = new Stock(); // Création du stock
+        // Initialisations
+        int ProducersNumber = 50; // Number of producers
+        int ConsumersNumber = 2; // Number of consumers
+
+        Stock stock = new Stock(); // Stock creation
 
 
-        // Création des producteurs
-        for (int i = 0; i < nbProducteurs; i++) {
-            Producteur producteur = new Producteur(stock); // Création du producteur i
-            producteur.start(); // Démarrage du producteur i
+        // Producers creation
+        for (int i = 0; i < ProducersNumber; i++) {
+            Producteur producteur = new Producteur(stock); // Creation of producer i
+            producteur.start(); // Start of producer i
         }
-        System.out.println(nbProducteurs + " producteurs ont été créés.");
+        System.out.println(ProducersNumber + " producers created.");
 
 
-        // Création des consommateurs
-        for (int i = 0; i < nbConsommateurs; i++) {
+        // Consumers creation
+        for (int i = 0; i < ConsumersNumber; i++) {
 
-            Consommateur consommateur = new Consommateur(stock); // Création du consommateur i
-            consommateur.start(); // Démarrage du consommateur i
+            Consommateur consommateur = new Consommateur(stock); // Creation of consumer i
+            consommateur.start(); // Start of consumer i
         }
-        System.out.println(nbConsommateurs + " consommateurs ont été créés.");
+        System.out.println(ConsumersNumber + " consumers created.");
 
 
     }
